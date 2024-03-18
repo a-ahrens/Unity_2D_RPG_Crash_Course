d4fd3e85afe7d20928e38a795a52da69d6623e1d
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2024-03-17 23:19:09 -0500

    Cleaned up file storage and Player script.

---
+++ RPG Udemy Course/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    private float xInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        CheckInput();
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        AnimatorControllers();
+
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0 ? true : false;
+
+        anim.SetBool("isMoving", isMoving);
+    }
+}
